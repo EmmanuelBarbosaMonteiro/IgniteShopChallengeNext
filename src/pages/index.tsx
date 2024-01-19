896d146bb5eb18877dc6c0b4c7b8d2a04100470c
Emmanuel <emmanuel-monteiro@hotmail.com>
2024-01-19 10:35:30 -0300

    feat: Added Stitches for global style settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <h1>Home</h1>
+    </>
+  )
+}
